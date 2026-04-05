aa4d88645650c8c4f9d5885b60d52218d95572f6
rohit mahajan <rohitmahajan6942089@gmail.com>
2026-04-05 17:48:50 +0530

    Enhance security of authentication cookies by adding httpOnly, secure, and sameSite attributes

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -7,7 +7,11 @@ const register = async (req, res) => {
     const { username, email, password } = req.body;
     const user = await registerUser({ username, email, password });
     const accessToken = generateToken(user);
-    res.cookie("token", accessToken);
+    res.cookie("token", accessToken,{
+      httpOnly: true,
+      secure: true, // Set to true in production
+      sameSite: "None", // Set to "Lax" or "Strict" in production
+    });
     return res.status(201).json({
       message: "User registered",
       user: {
@@ -28,7 +32,11 @@ const login = async (req, res) => {
     const { identifier, password } = req.body;
     const user = await loginUser(identifier, password);
     const accessToken = generateToken(user);
-    res.cookie("token", accessToken);
+    res.cookie("token", accessToken,{
+      httpOnly: true,
+      secure: true, // Set to true in production
+      sameSite: "None", // Set to "Lax" or "Strict" in production
+    });
     return res.status(200).json({
       message: "Login successful",
       user: {
